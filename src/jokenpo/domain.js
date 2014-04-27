dad5254d335189786481ae3b0470a66492c14fe4
Eduardo Baiao <eduardobaiao@gmail.com>
2014-04-27 19:47:49 -0300

    Criada lógica para o cálculo da jogada.

--- src/jokenpo/domain.js
+++ src/jokenpo/domain.js
@@ -3,16 +3,27 @@ game.module(
 )
 .body(function() {
     
-    Weapon = game.Class.extend({
-    });
-    
-    Player = game.Class.extend({
-    });
-    
-    CPU = game.Class.extend({
-    });
-    
-    JoKenPo = game.Class.extend({
-    });
+    JoKenPo = {
+        
+        Weapon: {
+            ROCK: 0,
+            PAPER: 1,
+            SCISSORS: 2
+        },
+        
+        Result: {
+            WIN: 1,
+            LOSE: 2,
+            DRAW: 0
+        },
+        
+        process: function(playerWeapon) {
+            return (3 + playerWeapon - this.cpuWeapon()) % 3;
+        },
+        
+        cpuWeapon: function() {
+            return Math.floor(Math.random() * 3);
+        }
+    };
 
 });
