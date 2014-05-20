25cdf495763747005e58388f93ffa8ba5606a5d5
Eduardo Baiao <eduardobaiao@gmail.com>
2014-05-20 15:38:28 -0300

    Criado controle reutilizável para os botões.

---
+++ pandajs/carnage-x/src/game/controls/button.js
@@ -0,0 +1,36 @@
+game.module(
+    'game.controls.button'
+)
+.body(function(){
+    
+    Button = game.Sprite.extend({
+        
+        scaleUp: function() {
+            this.scale.y = 1.1;
+            this.scale.x = 1.1;
+        },
+        
+        scaleNormal: function() {
+            this.scale.y = 1.0;
+            this.scale.x = 1.0;
+        },
+        
+        scaleDown: function() {
+            this.scale.y = 0.9;
+            this.scale.x = 0.9;
+        },
+        
+        init: function(id, x, y, onClick) {
+            this._super(id, x, y);
+            
+            this.anchor = {x: 0.5, y: 0.5};
+            this.interactive = true;
+            
+            this.mouseover = this.scaleUp.bind(this);
+            this.touchstart = this.mousedown = this.scaleDown.bind(this);
+            this.mouseout = this.touchend = this.mouseupoutside = this.touchendoutside = this.scaleNormal.bind(this);
+            
+            this.click = this.tap = onClick;
+        }
+    });
+});
--- pandajs/carnage-x/src/game/scenes/mainScene.js
+++ pandajs/carnage-x/src/game/scenes/mainScene.js
@@ -2,7 +2,8 @@ game.module(
     'game.scenes.mainScene'
 )
 .require(
-    'game.scenes.gameScene'
+    'game.scenes.gameScene',
+    'game.controls.button'
 )
 .body(function() {
     
@@ -10,60 +11,24 @@ game.module(
         
         init: function(){
             
-            var onMouseOver = function(){
-                this.scale.y = 1.2;
-                this.scale.x = 1.2;
-            };
-            var onMouseOut = function(){
-                this.scale.y = 1.0;
-                this.scale.x = 1.0;
-            };
-            
             var mainScreen = new game.Sprite("mainScreen");
             this.stage.addChild(mainScreen);
             
-            var btnStart = new game.Sprite("btnStart");
-            btnStart.position.set(750, 220);
-            btnStart.anchor = {x:0.5, y:0.5};
-            btnStart.interactive = true;
-            btnStart.mouseover = onMouseOver;
-            btnStart.mouseout = onMouseOut;
-            btnStart.click = function() {
-                game.system.setScene(GameScene);
-            };
-            this.stage.addChild(btnStart);
+            var btnStart =
+                new Button("btnStart", 750, 220,
+                    function() {
+                        game.system.setScene(GameScene);
+                    });
+                    
+            this.stage.addChild(btnStart);                                            
             
-            var btnHowToPlay = new game.Sprite("btnHowToPlay");
-            btnHowToPlay.position.set(750, 300);
-            btnHowToPlay.anchor = {x:0.5, y:0.5};
-            btnHowToPlay.interactive = true;
-            btnHowToPlay.mouseover = onMouseOver;
-            btnHowToPlay.mouseout = onMouseOut;
-            btnHowToPlay.click = function() {
-                game.system.setScene(SceneGame);
-            };
+            var btnHowToPlay = new Button("btnHowToPlay", 750, 300);
             this.stage.addChild(btnHowToPlay);
             
-            var btnHiScore = new game.Sprite("btnHiScore");
-            btnHiScore.position.set(750, 380);
-            btnHiScore.anchor = {x:0.5, y:0.5};
-            btnHiScore.interactive = true;
-            btnHiScore.mouseover = onMouseOver;
-            btnHiScore.mouseout = onMouseOut;
-            btnHiScore.click = function() {
-                game.system.setScene(SceneGame);
-            };          
+            var btnHiScore = new Button("btnHiScore", 750, 380);
             this.stage.addChild(btnHiScore);
             
-            var btnCredits = new game.Sprite("btnCredits");
-            btnCredits.position.set(750, 460);
-            btnCredits.anchor = {x:0.5, y:0.5};
-            btnCredits.interactive = true;
-            btnCredits.mouseover = onMouseOver;
-            btnCredits.mouseout = onMouseOut;
-            btnCredits.click = function() {
-                game.system.setScene(SceneGame);
-            };
+            var btnCredits = new Button("btnCredits", 750, 460);
             this.stage.addChild(btnCredits);
         }
     });
