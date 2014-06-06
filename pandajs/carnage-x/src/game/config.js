ffb4a3d0099ecce4979599945857493015682789
Eduardo Baiao <eduardobaiao@gmail.com>
2014-06-06 03:34:38 -0300

    Movida a configuração do loader para o config.js. Corrigido bug na configuração do mobile.

--- pandajs/carnage-x/src/game/config.js
+++ pandajs/carnage-x/src/game/config.js
@@ -8,6 +8,13 @@ pandaConfig = {
         rotateMsg: 'Por favor, gire seu dispositivo.'
     },
     mobile: {
-        orientation: 'portrait'
+        system: {
+            orientation: 'portrait'
+        }
+    },
+    loader: {
+        bgColor: 0x1D0006,
+        barBg: 0x4A030F,
+        barColor: 0xAD8473
     }
 };
--- pandajs/carnage-x/src/game/main.js
+++ pandajs/carnage-x/src/game/main.js
@@ -13,10 +13,6 @@ game.module(
 )
 .body(function(){
     
-    game.Loader.bgColor = 0x1D0006;
-    game.Loader.barBg = 0x4A030F;
-    game.Loader.barColor = 0xAD8473;
-    
     game.start(MainScene);
     
     //game.start(GameScene);
