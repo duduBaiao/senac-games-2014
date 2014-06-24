000d8e31c849abf5aed8880ca8787516264510f3
Eduardo Baiao <eduardobaiao@gmail.com>
2014-06-24 10:54:02 -0300

    No mobile estava forçando portrait. Agora o padrão é landscape, sempre.

--- pandajs/carnage-x/src/game/config.js
+++ pandajs/carnage-x/src/game/config.js
@@ -7,11 +7,6 @@ pandaConfig = {
         resizeToFill: true,
         rotateMsg: 'Por favor, gire seu dispositivo.'
     },
-    mobile: {
-        system: {
-            orientation: 'portrait'
-        }
-    },
     loader: {
         bgColor: 0x1D0006,
         barBg: 0x4A030F,
